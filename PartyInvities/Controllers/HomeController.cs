d30ffabf913eed4037e3b5a0697c02ccc950362c
kaizen23 <damianis2@poczta.onet.pl>
2016-11-28 19:07:34 +0100

    Add project files.

---
+++ PartyInvities/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using PartyInvities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvities.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 17 ? "Dzień dobry" : "Dobry wieczor";
+            return View();
+        }
+        [HttpGet]
+        public ActionResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+    }
+}
---
+++ PartyInvities/Models/GuestResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvities.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Proszę podać swoje imię i nazwisko")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Proszę podać maila")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Proszę podać prawidłowy email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Proszę podać numer telefonu")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Proszę podać czy weźmiesz udział")]
+        public bool? WillAttend  { get; set; }
+
+    }
+}
